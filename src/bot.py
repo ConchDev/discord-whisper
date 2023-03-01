9a750a3c4ec678e4be06fa6af18a7846e3cacf21
ConchDev <unsoughtconch@gmail.com>
2023-03-01 17:28:22 -0500

    feat: add recording, connecting, stopping

--- src/bot.py
+++ src/bot.py
@@ -6,13 +6,18 @@ dotenv.load_dotenv()
 
 class Bot(discord.Bot):
     def __init__(self):
-        ...
+        super().__init__(debug_guilds=[733467617804812409])
+        self._connections = {}
     
-    def load_extensions(self):
-        self.load_extensions("cogs.record")
+    @property
+    def connections(self):
+        return self._connections
+    
+    def load_cogs(self):
+        self.load_extension("cogs.record")
 
-    def run():
-        bot.load_extensions()
+    def run(self):
+        bot.load_cogs()
         super().run(os.getenv("TOKEN"))
 
 if __name__ == "__main__":
--- src/cogs/record.py
+++ src/cogs/record.py
@@ -1,5 +1,6 @@
 import discord
 import typing
+import datetime
 
 if typing.TYPE_CHECKING:
     from ..bot import Bot
@@ -8,6 +9,43 @@ class Record(discord.Cog):
     def __init__(self, bot: "Bot") -> None:
         self.bot = bot
         super().__init__()
+    
+    async def after_recording(self, sink: discord.sinks.WaveSink, channel: discord.VoiceChannel):
+        file = [discord.File(audio.file, f"{user_id}.{sink.encoding}") for user_id, audio in sink.audio_data.items()]
+        await channel.send("Here is your recording.", files=file)
+
+
+    @discord.command()
+    async def record(self, ctx: discord.ApplicationContext):
+        voice = ctx.author.voice
+
+        if not voice:
+            return await ctx.respond("You are not in a voice channel. Please connect to one and try again.")
+        
+        vc = await voice.channel.connect()
+        self.bot.connections.update({ctx.guild.id: {"vc": vc, "time": datetime.datetime.now()}})
+
+        vc.start_recording(
+            discord.sinks.WaveSink(
+            filters={"max_size": 25000000}
+            ),
+            self.after_recording,
+            vc.channel
+        )
+
+        await ctx.respond("Recording started. Use `/stop` to stop recording.")
+
+    @discord.command()
+    async def stop(self, ctx: discord.ApplicationContext):
+        if ctx.guild.id not in self.bot.connections:
+            return await ctx.respond("There is no recording in progress.")
+        
+        vc = self.bot.connections[ctx.guild.id]["vc"]
+        vc.stop_recording()
+        await ctx.respond("Recording stopped. Use `/play` to play the recording.")
+
+
+
 
 def setup(bot: "Bot"):
     bot.add_cog(Record(bot))
